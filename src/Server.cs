ffd006ee57d57d9fc415168b72e2b9be521570a3
William McGonagle <wpmcgonagle@gmail.com>
2021-12-01 18:53:33 -0500

    ✨ Added Networking

--- src/Server.cs
+++ src/Server.cs
@@ -8,6 +8,8 @@ public class Server
     public Int32 port = 9090;
     public IPAddress localAddress;
 
+    public bool running = true;
+
     private TcpListener server;
 
     public Server()
@@ -17,7 +19,7 @@ public class Server
         localAddress = IPAddress.Parse("127.0.0.1");
 
         server = new TcpListener(localAddress, port);
-        
+
     }
 
     public Server(int _port, string _ip)
@@ -33,7 +35,43 @@ public class Server
     public void Listen()
     {
 
+        server.Start();
 
+        Byte[] bytes = new Byte[256];
+        String data = null;
+
+        while (running)
+        {
+
+            // WARNING: Blocking Call
+            TcpClient client = server.AcceptTcpClient();
+
+            data = null;
+
+            // Get the Stream Object
+            NetworkStream stream = client.GetStream();
+
+            // Read Stream into bytes
+            int i;
+            while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+            {
+
+                // Get Data as String
+                data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                Console.WriteLine("Received: {0}", data);
+
+                // Respond
+                byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                stream.Write(msg, 0, msg.Length);
+
+            }
+
+            // Close the Connection
+            client.Close();
+
+        }
+
+        server.Stop();
 
     }
 
