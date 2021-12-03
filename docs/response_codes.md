1ffe4b8e4e1c034fabc8c1fc5bb792fd9392e199
William McGonagle <wpmcgonagle@gmail.com>
2021-12-03 08:33:44 -0500

    💩 Added 200 Responses

---
+++ docs/response_codes.md
@@ -0,0 +1,31 @@
+# Response Codes
+
+## Table of Contents
+
+## 100
+
+### 100 - Continue
+
+### 101 - Switching Protocols
+
+## 200
+
+### 200 - Ok
+
+### 201 - Created
+
+### 202 - Accepted
+
+### 203
+
+### 204 - No Content
+
+### 205 - Reset Content
+
+### 206 - Partial Response
+
+## 300
+
+## 400
+
+## 500
--- src/utility/BasicResponse.cs
+++ src/utility/BasicResponse.cs
@@ -5,6 +5,10 @@ public class BasicResponse
 
     public const string OMTP_VERSION = "OMTP/0.9";
 
+    /// <summary>
+    /// Returns a "200 OK" Response.
+    /// </summary>
+    /// <returns>A "200 OK" Response.</returns>
     public static Response Okay()
     {
 
@@ -13,6 +17,111 @@ public class BasicResponse
         output.OmtpVersion = OMTP_VERSION;
         output.ResponseCode = 200;
         output.ResponseText = "OK";
+
+        output.Headers = new Dictionary<string, string>();
+
+        output.Body = "";
+
+        return output;
+
+    }
+
+    /// <summary>
+    /// Returns a "201 Created" Response.
+    /// </summary>
+    /// <returns>A "201 Created" Response.</returns>
+    public static Response Created()
+    {
+
+        Response output = new Response();
+
+        output.OmtpVersion = OMTP_VERSION;
+        output.ResponseCode = 201;
+        output.ResponseText = "Created";
+
+        output.Headers = new Dictionary<string, string>();
+
+        output.Body = "Created";
+
+        return output;
+
+    }
+
+    /// <summary>
+    /// Returns a "202 Accepted" Response.
+    /// </summary>
+    /// <returns>A "202 Accepted" Response.</returns>
+    public static Response Accepted()
+    {
+
+        Response output = new Response();
+
+        output.OmtpVersion = OMTP_VERSION;
+        output.ResponseCode = 202;
+        output.ResponseText = "Accepted";
+
+        output.Headers = new Dictionary<string, string>();
+
+        output.Body = "Accepted";
+
+        return output;
+
+    }
+
+    /// <summary>
+    /// Returns a "204 No Content" Response.
+    /// </summary>
+    /// <returns>A "204 No Content" Response.</returns>
+    public static Response NoContent()
+    {
+
+        Response output = new Response();
+
+        output.OmtpVersion = OMTP_VERSION;
+        output.ResponseCode = 204;
+        output.ResponseText = "No Content";
+
+        output.Headers = new Dictionary<string, string>();
+
+        output.Body = "";
+
+        return output;
+
+    }
+
+    /// <summary>
+    /// Returns a "205 Reset Content" Response.
+    /// </summary>
+    /// <returns>A "205 Reset Content" Response.</returns>
+    public static Response ResetContent()
+    {
+
+        Response output = new Response();
+
+        output.OmtpVersion = OMTP_VERSION;
+        output.ResponseCode = 205;
+        output.ResponseText = "Reset Content";
+
+        output.Headers = new Dictionary<string, string>();
+
+        output.Body = "";
+
+        return output;
+
+    }
+
+    /// <summary>
+    /// Returns a "206 Partial Content" Response.
+    /// </summary>
+    /// <returns>A "206 Partial Content" Response.</returns>
+    public static Response PartialContent()
+    {
+
+        Response output = new Response();
+
+        output.OmtpVersion = OMTP_VERSION;
+        output.ResponseCode = 206;
+        output.ResponseText = "Partial Content";
 
         output.Headers = new Dictionary<string, string>();
 
